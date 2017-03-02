cd7283f31151e0a89ac9d9ed09b244ffa9d25593
Hieu Nguyen <hnnguyen45@gmail.com>
2017-03-01 23:48:55 -0800

    Added state to our tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,95 +1,51 @@
 class TasksController < ApplicationController
-
   before_action :authenticate_user!
-
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-
-
 
   respond_to :html
 
-
-
   def index
-
-    @tasks = current_user.tasks
-
+    @to_do = current_user.tasks.where(state: "to-do")
+    @doing = current_user.tasks.where(state: "doing")
+    @done = current_user.tasks.where(state: "done")
     respond_with(@tasks)
-
   end
-
-
 
   def show
-
     respond_with(@task)
-
   end
-
-
 
   def new
-
     @task = Task.new
-
     respond_with(@task)
-
   end
-
-
 
   def edit
-
   end
-
-
 
   def create
-
     @task = current_user.tasks.new(task_params)
-
     @task.save
-
     respond_with(@task)
-
   end
-
-
 
   def update
-
     @task.update(task_params)
-
     respond_with(@task)
-
   end
-
-
 
   def destroy
-
     @task.destroy
-
     respond_with(@task)
-
   end
 
-
-
   private
-
     def set_task
-
       @task = Task.find(params[:id])
-
     end
 
-
-
     def task_params
-
-      params.require(:task).permit(:content)
-
+      params.require(:task).permit(:content, :state)
     end
 
 end
